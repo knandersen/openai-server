3c424021e000779756f5f8359979ead4329ffcfd
Kevin Nørby Andersen <things@kevinandersen.dk>
2023-01-04 23:20:00 +0100

    add CORS allows

--- app.js
+++ app.js
@@ -2,7 +2,20 @@ require('dotenv').config()
 const express = require("express");
 const { Configuration, OpenAIApi } = require("openai");
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 3001;
+
+// CORS
+
+app.use(cors())
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+})
+
+// OpenAI config
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_SECRET,
@@ -15,6 +28,8 @@ const completionTemplate = {
   temperature: 0.6,
   max_tokens: 35,
 }
+
+// Endpoints
 
 app.get("/connect", (req, res) => res.type('html').send(`<p>Connection established!</p>`));
 
