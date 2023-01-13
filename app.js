30fb3c0a75be800c50d2ebdb007e326696a59c89
Kevin Nørby Andersen <things@kevinandersen.dk>
2023-01-13 12:39:35 +0100

    switching from davinci to curie model for cheaper and faster

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const completionTemplate = {
-  model: "text-davinci-003",
+  model: "text-curie-001",
   prompt: "",
   temperature: 0.6,
   max_tokens: 35,
